6afb61be7078a342114fa318288e2c9114422206
roshan-04 <roshanyadav111004@gmail.com>
2025-07-31 19:11:47 +0530

    Implements a todo list, which takes task input and displays it

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,30 @@
 <template>
-  <q-page class="testing">
-    <h4>{{ message }}</h4>
+  <q-page class="q-pa-md">
+    <h5>Todo List</h5>
 
-    <!-- you use v-model here to binding the data and message -->
-    <input v-model="message" label="Type something" filled class="testing" />
-    <q-btn color="primary" label="Clear" v-on:click="message = ''" />
+    <q-input v-model="newTask" label="Add Task" filled @keyup.enter="addTask" />
+    <q-btn label="Add" @click="addTask" color="secondary" class="q-my-md" />
+
+    <q-list bordered class="q-mt-md" v-if="todos.length">
+      <q-item v-for="(todo, index) in todos" :key="index">
+        <q-item-section>{{ index + 1 }}. {{ todo }}</q-item-section>
+      </q-item>
+    </q-list>
+
+    <div v-else class="text-grey">No tasks added yet.</div>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const message = ref("Welcome Roshan Yadav");
-</script>
+const newTask = ref("");
+const todos = ref([]);
 
-<style></style>
+function addTask() {
+  if (newTask.value.trim()) {
+    todos.value.push(newTask.value);
+    newTask.value = "";
+  }
+}
+</script>
