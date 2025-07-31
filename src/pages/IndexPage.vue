8de58fea0cd340db9af935f8ae8a58aeb7789455
roshan-04 <roshanyadav111004@gmail.com>
2025-07-31 19:07:50 +0530

    Stared learning quasar, Implemented the input field, whatever is entered is showed immediately and clear

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="testing">
+    <h4>{{ message }}</h4>
+
+    <!-- you use v-model here to binding the data and message -->
+    <input v-model="message" label="Type something" filled class="testing" />
+    <q-btn color="primary" label="Clear" v-on:click="message = ''" />
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Welcome Roshan Yadav");
+</script>
+
+<style></style>
